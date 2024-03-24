b8d38df2bf59d6827c804c99afd2a93d65d6b351
wooseoknam <ws9706@gmail.com>
2024-03-24 22:34:33 +0900

    init

---
+++ backend/src/main/java/hanyang/shortcut/backend/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package hanyang.shortcut.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api")
+    public String index() {
+        return "aaaaaaaaaaaaaa";
+    }
+}
